bdcc47d6d5e55db31f0f36d6e95810ef07173fd9
misoton665 <misoton998@gmail.com>
2016-11-24 07:28:43 +0900

    first commit

---
+++ src/Hello.tsx
@@ -0,0 +1,10 @@
+// export interface Props {
+//   content: string;
+// }
+
+export default class Hello extends React.Component<{}, {}> {
+
+  public render(){
+    return <h1>hgehoge</h1>;
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import Hello from "./Hello";
+
+// interface Props {
+//   content: string;
+// }
+
+// class Hello extends React.Component<{}, {}> {
+//   // style: any;
+//   // constructor() {
+//   //   super()
+//   //   this.style = {
+//   //     title: {
+//   //       color: "#ddd",
+//   //       textAlign: "center"
+//   //     }
+//   //   }
+//   // }
+
+//   public render(){
+//     return <h1>hgehoge</h1>;
+//   }
+// }
+
+
+ReactDOM.render(<Hello/>, document.getElementById('app'));
